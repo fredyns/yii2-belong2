142d43ca7acdaadb4c979d693682a92f6231bd30
Fredy Nurman Saleh <email@fredyns.net>
2017-04-24 19:46:44 +0700

    belong to model script

--- BelongToModelBehavior.php
+++ BelongToModelBehavior.php
@@ -2,15 +2,22 @@
 
 namespace fredyns\belong2;
 
+use Yii;
+use yii\base\InvalidConfigException;
+use yii\behaviors\AttributeBehavior;
 use yii\db\ActiveRecord;
+use yii\helpers\ArrayHelper;
 
 /**
  * Description of BelongToModelBehavior
  *
  * @author Fredy Nurman Saleh <email@fredyns.net>
  */
-class BelongToModelBehavior
+class BelongToModelBehavior extends AttributeBehavior
 {
+    public $attribute;
+    public $belongTo;
+    public $mirrorAttributes = [];
 
     /**
      * sample use of this class in Model::behaviors()
@@ -32,7 +39,7 @@ class BelongToModelBehavior
                     'attribute-2' => 'value-2',
                 ],
                 // use closure to define belonging model. useful when need to update current belonging model.
-                'belongTo' => function($thisBehavior, $ownerModel) {
+                'belongTo' => function($ownerModel, $thisBehavior) {
                     //  // when updating owner model
                     //  if ($ownerModel->isNewRecord == false) {
                     //      // if there is previous belonging key
@@ -67,5 +74,91 @@ class BelongToModelBehavior
                 ],
             ],
         ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        if (empty($this->attribute)) {
+            throw new InvalidConfigException("Related attribute must be set.");
+        }
+
+        if (empty($this->belongTo)) {
+            throw new InvalidConfigException("Belonging model class must be set.");
+        } elseif (!is_string($this->belongTo) && !is_array($this->belongTo) && !($this->belongTo instanceof \Closure)) {
+            throw new InvalidConfigException("Belonging model setup invalid.");
+        }
+
+        if ($this->mirrorAttributes && !is_array($this->mirrorAttributes)) {
+            throw new InvalidConfigException("Mirror attribute must be an array.");
+        }
+
+        if (empty($this->attributes)) {
+            $this->attributes = [
+                ActiveRecord::EVENT_BEFORE_INSERT => $this->attribute,
+                ActiveRecord::EVENT_BEFORE_UPDATE => $this->attribute,
+            ];
+        }
+    }
+
+    /**
+     * Evaluates the value of the user.
+     * The return result of this method will be assigned to the current attribute(s).
+     * @param Event $event
+     * @return mixed the value of the user.
+     */
+    protected function getValue($event)
+    {
+        $value = ArrayHelper::getValue($this->owner, $this->attribute);
+
+        if (empty($value)) {
+            return NULL;
+        } elseif (is_numeric($value)) {
+            return $value;
+        } else {
+            $belongingModel = $this->getBelongingModel();
+            $belongingModel = $this->copyAttributes($belongingModel);
+
+            return $belongingModel->save(FALSE) ? $belongingModel->id : null;
+        }
+    }
+
+    /**
+     * get belonging model instance
+     *
+     * @return ActiveRecord
+     */
+    public function getBelongingModel()
+    {
+        $belongTo = $this->belongTo;
+
+        if ($belongTo instanceof \Closure) {
+            return $belongTo($this->owner, $this);
+        }
+
+        return Yii::createObject($belongTo);
+    }
+
+    /**
+     * copy this owner attributes to model
+     *
+     * @param ActiveRecord $model
+     * @return ActiveRecord
+     */
+    public function copyAttributes($model)
+    {
+        foreach ($this->mirrorAttributes as $targetAttribute => $sourceAttribute) {
+            if (is_integer($targetAttribute)) {
+                $targetAttribute = $sourceAttribute;
+            }
+
+            $model->{$targetAttribute} = $this->owner->{$sourceAttribute};
+        }
+
+        return $model;
     }
 }
